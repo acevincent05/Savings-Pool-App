ee5e4417bbd391bce4263fdb7377e7069bfb7bbd
acevincent05 <gamitinacevincent@gmail.com>
2026-05-25 14:29:36 +0800

    # added models

---
+++ Backend/Models/User.cs
@@ -0,0 +1,15 @@
+// using System.ComponentModel.DataAnnotations;
+
+// namespace Backend.Models
+// {
+//     public class StudentPrograms
+//     {
+//         public int Id { get; set; }
+
+//         [Required]
+//         public string ProgramName { get; set; } = string.Empty;
+
+//         public ICollection<Student> Students { get; set; } = new List<Student>();
+//         public ICollection<Section> Sections { get; set; } = new List<Section>();
+//     }
+// }
