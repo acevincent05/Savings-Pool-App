07fbe3b4175425b90fa43280aeb46a0e87035576
Shalamander <176278148+Shalamanter@users.noreply.github.com>
2026-05-25 16:11:55 +0800

    Add React JS frontend to the project

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf68f79a2abd37f82ee0cd0e47a9c7b597557d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e513dc9d5dff569e222c7d480f772d8578744071")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
