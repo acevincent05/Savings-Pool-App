ce14a64b3dc4146cf978d4d0702903a9495b274f
gamitinacevincent@gmail.com <gamitinacevincent@gmail.com>
2026-05-26 13:49:22 +0800

    #

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ae6d124a0015be9b411bca66bdaad410db2bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb3ad332025db30e9e5d660c71b849824d83d49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
